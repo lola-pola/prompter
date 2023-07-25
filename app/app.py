17a57e3018eec996dd74c8402a220dfb5f785711
shdowofdeath <elhayefrat@gmail.com>
2023-07-25 10:58:15 +0300

    sdasd
    Signed-off-by: shdowofdeath <elhayefrat@gmail.com>

--- app/app.py
+++ app/app.py
@@ -20,7 +20,6 @@ def csv_to_dict(file_path):
 
 
 def chatterbot(question, context,max_tokens=2000,temperature=1,engine="gpt3"):
-        print(question,context,max_tokens,temperature,engine)
         response = openai.ChatCompletion.create(
             engine=engine,
             messages=[{"role":"system","content":context},
@@ -41,9 +40,15 @@ def app():
     st.title("OpenAI Prompts Learning Challenge") 
     
     with st.sidebar:
-            data = csv_to_dict("prompts.csv")
-            selection = st.selectbox("what prompt?",list(data.keys()))
-            context = st.text_area("few shot context", height=200, key="you are a bot very nice of a israeli startup",value=data[selection])
+            usage = st.selectbox("usage", ["-- select ---", "act", "generate"])
+            if usage == "act":
+                data = csv_to_dict("prompts.csv")
+                selection = st.selectbox("what prompt?",list(data.keys()))
+            else:
+                data = csv_to_dict("examples-data.csv")
+                selection = st.selectbox("what prompt?",list(data.keys()))
+                prompt_example = data[selection]
+            context = st.text_area("few shot context", height=300, key="you are a bot very nice of a israeli startup",value=data[selection])
             max_tokens = st.slider("Max tokens", value=2000, key="max_tokens", min_value=16000, max_value=20000)
             temperature = st.slider("Temperature", value=0.5, key="temperature",max_value=1.0, min_value=0.0)
             engine = st.text_input("Engine", "gpt-35-turbo-16k", key="gpt-35-turbo-16k")
@@ -53,7 +58,7 @@ def app():
             openai.api_type = "azure"
             openai.api_base = st.text_input("API base", value="https://coca-cola.openai.azure.com/", key="api_base")
             openai.api_version = st.text_input("api version",value="2023-03-15-preview")
-            openai.api_key = st.text_input('azure openai key', key="KEY_AZURE_AI", value="a5d6336081ee3470e95e04c2ff1488f78", type="password") 
+            openai.api_key = st.text_input('azure openai key', key="KEY_AZURE_AI", value="5d6336081ee3470e95e04c2ff1488f78", type="password") 
             if st.checkbox("Submit", key="submit"):
                 st.success("Submitted!")
 
@@ -73,20 +78,26 @@ def app():
         st.session_state['past'] = []
         
         
+    # if user_input is not None:
+    if usage == "act":
+        user_input=st.text_input("You:",key='input')
 
-    user_input=st.text_input("You:",key='input')
-
-    if user_input:
-        context += str(st.session_state['past'] )
-        output=chatterbot(user_input,context,max_tokens,temperature,engine)
-        st.session_state['past'].append(user_input)
-        st.session_state['generated'].append(output)
-        if st.session_state['generated']:
-            for i in range(len(st.session_state['generated'])-1, -1, -1):
-                message(st.session_state["generated"][i], key=str(i))
-                message(st.session_state['past'][i], is_user=True, key=str(i) + '_user') 
-                st.session_state.generated = ''
-
+        if user_input:
+            context += str(st.session_state['past'] )
+            output=chatterbot(user_input,context,max_tokens,temperature,engine)
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+            if st.session_state['generated']:
+                for i in range(len(st.session_state['generated'])-1, -1, -1):
+                    message(st.session_state["generated"][i], key=str(i))
+                    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user') 
+                    st.session_state.generated = ''
+    elif usage == "generate":
+        st.write(prompt_example)
+        with st.spinner('waiting for response...'):
+            st.write(chatterbot(prompt_example,context,max_tokens,temperature,engine))
+    else:
+        st.markdown('select what prompt usage')
 
 
 def main():
